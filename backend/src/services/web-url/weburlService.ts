2542f7cc7d045ea26682762b9276f1c22cbd657f
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-07-29 18:18:52 +0530

    Add web url route and refactor controller and services

--- backend/src/services/web-url/weburlService.ts
+++ backend/src/services/web-url/weburlService.ts
@@ -1,13 +1,26 @@
 import puppeteer from 'puppeteer';
 
 
-export const webUrlContentService = async (url:string) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    const content = page.evaluate(() => {
-        return document.body.innerText;
-    })
-    await browser.close();
-    return content;
+export const webUrlContentService = async (url:string):Promise<string> => {
+
+    const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+
+    try{
+        const page = await browser.newPage();
+        await page.goto(url);
+        const content = page.evaluate(() => {
+            return document.body.innerText;
+        })
+        await browser.close();
+        return content;
+
+    } catch (error) {
+        console.error("webUrlContentService error ", error);
+        throw error;
+    }
+    finally{
+        await browser.close();
+    }
 }
